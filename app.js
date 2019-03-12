eb6facb934f75306d4bcdade6ecb5935c847cece
Vipin Mantri <mantrivipin17@gmail.com>
2019-03-12 12:50:42 +0530

    added html and js code

---
+++ app.js
@@ -0,0 +1,9 @@
+var container = document.querySelector(".img-sample");
+
+if (container) {
+  container.addEventListener("load", function() {
+    var elm = document.createElement("div");
+    elm.innerHTML = "image loaded";
+    document.body.appendChild(elm);
+  });
+}
